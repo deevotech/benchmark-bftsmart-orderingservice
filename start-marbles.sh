182b075d981fa6f019894c313857c106498aec8e
Quang Phan <pquocquang1102@gmail.com>
2018-10-18 16:06:50 +0700

    fix logs

--- start-marbles.sh
+++ start-marbles.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p data/logs_1
-sudo rm -rf data/logs_1/*
+mkdir -p data/logs_2
+sudo rm -rf data/logs_2/*
 
 export COMPOSE_PROJECT_NAME=net && docker-compose -f compose/client.yaml up
